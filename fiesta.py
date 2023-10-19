e41d797d2f339e0a2ea485549f27c4e0668ac650
Meli-Lopez <129072033+Meli-Lopez@users.noreply.github.com>
2023-10-19 08:20:18 -0500

    Update fiesta.py

--- fiesta.py
+++ fiesta.py
@@ -3,46 +3,54 @@ invitadosRechazados = []
 
 # Requisitos de ingreso
 edad = 16
-vestimentaHawaiana = True
-porteArmas = False
+contadorAceptados = 0
+contadorRechazados = 0
 
+bienvenida = input("Bienvenido a la fiesta, ¿deseas continuar? (si/no): ")
+while bienvenida.lower() == "si":
+    nombreUsuario = input("Ingresa tu nombre: ")
+    edadUsuario = int(input("Ingresa tu edad: "))
+    vestimenta = input("¿Tu disfraz es hawaiano o tiene algún accesorio hawaiano? (Si o No): ").strip().lower()
 
-nombreUsuario = input("Ingresa tu nombre: ")
-edadUsuario = int(input("Ingresa tu edad: "))
-vestimenta = input("¿Tu disfraz es hawaiano o tiene algun accesorio hawaiano? (Si o No): ").strip().lower()
-vestimentaUsuario = input("Escribe cual de estas opciones llevas en tu disfraz: (camisa de flores, falda de hierbas, pantaloneta hawaiana, collar de flores): ").strip().lower()
-arma = input("¿Portas algun tipo de arma? (Si o No): ").strip().lower()
+    cumpleVestimenta = False
+    if vestimenta == "si":
+        vestimentaUsuario = input("Escribe cual de estas opciones llevas en tu disfraz: (camisa de flores, falda de hierba, pantaloneta hawaiana, collar de flores): ").strip().lower()
+        vestimentaHawaiana = ["camisa de flores", "falda de hierba", "pantaloneta hawaiana", "collar de flores"]
+        for i in vestimentaHawaiana:
+            if i in vestimentaUsuario:
+                cumpleVestimenta = True
+                break
 
-cumpleVestimenta = False
-vestimentaHawaiana = ["camisa de flores", "falda de hierba", "pantaloneta hawaiana", "collar de flores"]
+    arma = input("¿Portas algun tipo de arma? (Si o No): ").strip().lower()
 
-for i in vestimentaHawaiana:
-    if i in vestimentaUsuario:
-        cumpleVestimenta = True
-        break
-
-if edadUsuario >= edad and vestimenta == "si" and arma == "no" and cumpleVestimenta:
-    invitadosAceptados.append({"Nombre": nombreUsuario, "Edad": edadUsuario})
-    print("¡Bienvenido a la fiesta hawaiana, DISFRUTA AL MAXIMO :D!")
-else:
     causaDeRechazo = []
     if edadUsuario < edad:
-        causaDeRechazo.append("No cumple con la edad minima para el ingreso a la fiesta segun los requisitos.")
-    if vestimenta != "si":
-        causaDeRechazo.append("Su disfraz no es adecuado para la fiesta, por lo tanto no puede ingresar.")
-    if arma != "no":
-        causaDeRechazo.append("El ingreso de armas es prohibido en la fiesta y usted porta un arma, por lo tanto no puede ingresar.")
+        causaDeRechazo.append(f"No cumples con la edad minima requerida ({edad} años) para el ingreso a la fiesta.")
+    if vestimenta != "si" or not cumpleVestimenta:
+        causaDeRechazo.append("Tu disfraz no es adecuado para la fiesta, por lo tanto no puedes ingresar.")
+    if arma == "si":
+        causaDeRechazo.append("El ingreso de armas esta prohibido en la fiesta y estas portando un arma, por lo tanto no puedes ingresar.")
+
+    if not causaDeRechazo:
+        contadorAceptados += 1
+        invitadosAceptados.append({"Nombre": nombreUsuario, "Edad": edadUsuario})
+        print("¡Bienvenido a la fiesta hawaiana, DISFRUTA AL MAXIMO! :D")
     else:
-        print("No puede ingresar a la fiesta")
+        contadorRechazados += 1
+        invitadosRechazados.append({"Nombre": nombreUsuario, "Edad": edadUsuario, "Motivo de Rechazo": causaDeRechazo})
+        print("Lo siento, no cumples con alguno de los requisitos para ingresar a la fiesta hawaiana.")
 
-'''
-invitadosRechazados.append({"Nombre": nombreUsuario, "Edad": edadUsuario, "El motivo por el cual usted no puede ingresar a la fiesta es": causaDeRechazo})    
-print("Lo siento, no cumples con alguno de los requisitos para ingresar a la fiesta hawaiana.")
+    bienvenida = input("¿Deseas continuar? (si/no): ")
+    while bienvenida.lower()=="no":
+        break
 
-
-print("\nInforme de usuarios que han ingresado a la fiesta:")
+print("Informe de usuarios aceptados a la fiesta: ")
 for invitado in invitadosAceptados:
     print(f"Nombre: {invitado['Nombre']}, Edad: {invitado['Edad']}")
+
+print("Informe de usuarios cuyo ingreso a la fiesta ha sido rechazado: ")
+for invitado in invitadosRechazados:
+    print(f"Nombre: {invitado['Nombre']}, Edad: {invitado['Edad']}, Razones de Rechazo: {', '.join(invitado['Motivo de Rechazo'])}")
 
 print("\nInforme de usuarios que no han ingresado a la fiesta:")
 for invitado in invitadosRechazados:
